cfb7565d73e7a813dae51374cd08a5580cb8eb20
Rick42Morty <evgenevgen00@mail.ru>
2018-09-30 00:34:51 +0300

    add sortByProto

--- src/index.js
+++ src/index.js
@@ -11,14 +11,16 @@ const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = (par) => {};
+const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {
     let obj = new Number(10);
     return obj;
 };
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+    return arr.sort((a, b) => a.__proto__ - b.__proto__);
+}
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
