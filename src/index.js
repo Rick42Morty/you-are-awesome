b9921ac621259cc32e6b23a291eddcf723051215
Rick42Morty <evgenevgen00@mail.ru>
2018-09-29 21:09:11 +0300

    add 3 func

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 // DO WHATEVER YOU WANT HERE
 
 const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
+const createNotEnumerableProperty = (par) => {
+   let prop = Symbol(par);
+   return prop;
+};
 const createProtoMagicObject = () => {};
 const incrementor = () => {};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
+const returnBackInSecond = (par) => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => {
+    let obj = new Number(10);
+    return obj;
+};
 const toBuffer = () => {};
 const sortByProto = () => {};
 
